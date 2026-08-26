a699671cc576ae0e4bac56612ddf7ec2adc3b741
Ranjita Naik <nranjita071@gmail.com>
2026-08-26 23:14:40 +0530

    variables and operators

---
+++ problem1.py
@@ -0,0 +1,71 @@
+ #import os
+"""import os
+directory_path = 'C:/Users/nranj/OneDrive/Desktop'
+contents = os.listdir(directory_path)
+for item in contents:
+    print (item)
+"""
+
+
+#operators
+"""a = 6
+b = 6
+a += 2
+b -= 2
+print(a)
+print(b)
+d =7<8
+print(d)
+e = True and False
+print(e)
+f = True or False
+print (f)
+"""
+
+#type 
+"""a="hello"
+b=type(a)
+print(b)
+c=1000.90
+print(type(c))
+ 
+ #converting
+d=int(c)
+print(type(d))
+a= 31
+b=type(a)
+print(b)
+c =str(a)
+print(c)"""
+
+#input functions
+"""a = input("Enter your name: ")
+print("Hello, "+a+", Welcome to the program.")
+
+a= input("Enter your age: ")
+b= input("Enter your age: ")
+print("age is:", a+b)
+
+c= int(input("Enter your age: "))
+d= int(input("Enter your age: "))
+print("age is:", c+d)
+"""
+#practicecproblems
+#1
+"""a=12346
+b=76
+print("reminder is:", a%b)"""
+#2
+"""a=input("Enter your name: ")
+print (type(a))"""
+#3
+"""a= int(input("Enter first numer 1: "))
+b= int(input("Enter second number 2: "))
+print("a is greater than b is:",a>b)"""
+#4
+"""a= int(input("Enter first number 1: "))
+b= int(input("Enter second number 2: "))
+print ("average of two numberr is :",(a+b)/2)"""
+# 5
+"""a= int(input("Enter first number 1: "))
+print("square of number is:",a*a)"""
